38ac311048e0395c2778ddcbe3a8ee81493eb169
Alex793x <Alex793x@stud.kea.dk>
2022-09-25 18:11:45 +0200

    Added constructor and Getter / Setter

--- src/Kid.java
+++ src/Kid.java
@@ -1,3 +1,32 @@
 public class Kid {
-    String name;
+    private String name;
+    int yearOfBirth;
+
+    // Getter -------------
+    public String getName() {
+        return name;
+    }
+
+    // Setter -------------
+    void setName(String newName) {
+        this.name = newName;
+    }
+
+    // Constructors --------
+    public Kid() {
+
+    }
+
+    public Kid(String kidName) {
+        this.name = kidName;
+    }
+
+
+    @Override
+    public String toString() {
+        return "This kids " +
+                "name is " + name + "\n" +
+                "and year of birth is " + yearOfBirth;
+    }
 }
+
--- src/Test.java
+++ src/Test.java
@@ -7,17 +7,48 @@ public class Test {
         Kid k4 = new Kid();
         Kid k5 = new Kid();
 
-        k1.name = "Johnny";
-        k2.name = "Patrick";
-        k3.name = "Alex";
-        k4.name = "Tom";
-        k5.name = "Frederick";
+        // Kid constructor with name
+        Kid k6 = new Kid("Henrick");
+        Kid k7 = new Kid("Jasmin");
+        Kid k8 = new Kid("Fiona");
+        Kid k9 = new Kid("Julianna");
+        Kid k10 = new Kid("Ann-Sophie");
 
-        System.out.println("Kid1s name is: " + k1.name);
-        System.out.println("Kid2s name is: " + k2.name);
-        System.out.println("Kid3s name is: " + k3.name);
-        System.out.println("Kid4s name is: " + k4.name);
-        System.out.println("Kid5s name is: " + k5.name);
+        k1.setName("Johnny");
+        k1.yearOfBirth = 2010;
+        // ---------------------
+        k2.setName("Patrick");
+        k2.yearOfBirth = 2012;
+        // ---------------------
+        k3.setName("Alex");
+        k3.yearOfBirth = 2008;
+        // ---------------------
+        k4.setName("Tom");
+        k4.yearOfBirth = 2000;
+        // ---------------------
+        k4.setName("Frederick");
+        k5.yearOfBirth = 2016;
+
+        System.out.println("Kid1s name is: " + k1.getName());
+        System.out.println("Kid2s name is: " + k2.getName());
+        System.out.println("Kid3s name is: " + k3.getName());
+        System.out.println("Kid4s name is: " + k4.getName());
+        System.out.println("Kid5s name is: " + k5.getName());
+
+        System.out.println("----------------------------");
+
+        System.out.println(k1);
+        System.out.println("----------------------------");
+        System.out.println(k2);
+        System.out.println("----------------------------");
+        System.out.println(k3);
+        System.out.println("----------------------------");
+        System.out.println(k4);
+        System.out.println("----------------------------");
+        System.out.println(k5);
+
+        System.out.println("----------------------------");
+
 
 
     }
