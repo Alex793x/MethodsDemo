8dbe54ff4566baabe85ffa05f8c2fdb2bdc51776
Alex793x <Alex793x@stud.kea.dk>
2022-09-25 17:53:39 +0200

    First Commit

---
+++ src/Test.java
@@ -0,0 +1,24 @@
+public class Test {
+
+    public static void main(String[] args) {
+        Kid k1 = new Kid();
+        Kid k2 = new Kid();
+        Kid k3 = new Kid();
+        Kid k4 = new Kid();
+        Kid k5 = new Kid();
+
+        k1.name = "Johnny";
+        k2.name = "Patrick";
+        k3.name = "Alex";
+        k4.name = "Tom";
+        k5.name = "Frederick";
+
+        System.out.println("Kid1s name is: " + k1.name);
+        System.out.println("Kid2s name is: " + k2.name);
+        System.out.println("Kid3s name is: " + k3.name);
+        System.out.println("Kid4s name is: " + k4.name);
+        System.out.println("Kid5s name is: " + k5.name);
+
+
+    }
+}
